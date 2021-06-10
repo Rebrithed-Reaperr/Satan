a13b737ffc573e04ce226da6b9016d1acb1cea82
Rebrithed-Reaperr <85047722+Rebrithed-Reaperr@users.noreply.github.com>
2021-06-10 15:03:09 -0500

    Add files via upload

---
+++ satan.sh
@@ -0,0 +1,47 @@
+@echo off
+mode con lines=28 cols=50
+chcp 65001
+color 0b
+title Satan
+echo Welcome login Satanist
+echo.
+echo 41;37m (                         
+echo 41;37m )\ )         )            
+echo 41;37m(()/(    ) ( /(   )        
+echo 41;37m /(_))( /( )\()| /(  (     
+echo 41;37m(_))  )(_)|_))/)(_)) )\ )  
+echo 41;37m/ __|((_)_| |_((_)_ _(_/(  
+echo 41;37m\__ \/ _` |  _/ _` | ' \)) 
+echo 41;37m|___/\__,_|\__\__,_|_||_|  
+echo.
+set /p user=Enter username:
+echo.
+set /p pass=Enter Password:
+if %user% == root if %pass% == root goto main
+echo Wrong Login, try again...
+timeout 2 >nul
+goto again
+cls
+echo Welcome %USERNAME%
+echo 41;37m (                         
+echo 41;37m )\ )         )            
+echo 45;37m(()/(    ) ( /(   )        
+echo 45;37m45;37m /(_))( /( )\()| /(  (     
+echo 45;37m(_))  )(_)|_))/)(_)) )\ )  
+echo 45;37m/ __|((_)_| |_((_)_ _(_/(  
+echo 41;37m\__ \/ _` |  _/ _` | ' \)) 
+echo 41;37m|___/\__,_|\__\__,_|_||_|  
+echo 41;37m Welcome to HELL 😈 😈 😈 
+echo.
+echo [+] 1 - DDoS
+echo [+] 2 - Tracking
+echo [+] 3 - Phone Tracing
+echo [+] 4 - Instagram
+set /p main=Choose your number:
+if %main% == 1 goto stressthem.to
+if %main% == 2 goto ip-tracker.org
+if %main% == 3 goto truepeoplesearch.com
+if %main% == 4 goto instagram.com/n0b0dy.python
+echo Wrong Number
+timeout 2 >nul
+goto mainlogo
